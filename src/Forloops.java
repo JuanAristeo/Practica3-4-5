14e4d07b4bd173aa3cfc18864ef791f8709bb104
JA <cruzaristeojuanluis@gmail.com>
2023-07-28 16:04:20 -0600

    Subo el archivo correcto de for

--- src/Forloops.java
+++ src/Forloops.java
@@ -4,13 +4,12 @@ public class Forloops {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		System.out.print("Escribe el numero : ");
-		int num = in.nextInt();
-		for ( int cont = 0 ; cont < 100; cont+=2) {
-			if(cont == num) {
-				continue;
-				}
-			System.out.println(cont);
-		}
-	}
-}
+		System.out.print("Escribe tu Nombre : ");
+		String name = in.next();
+	    for (int i = 0; i < name.length(); i++) {
+	    System.out.println(name.charAt(i));
+	    in.close();
+		}//for
+	}//main
+}//class
+
